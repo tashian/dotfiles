85aa2cf15e3b83a4a04493fd134c950948018ed3
Carl Tashian <carl@smallstep.com>
2022-05-18 10:31:09 -1000

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,5 +8,3 @@ if [ -f '/Users/carl/google-cloud-sdk/path.fish.inc' ]; . '/Users/carl/google-cl
 # we don't use the fish_add_path command here because 
 # it won't prepend to $PATH if we do.
 set -g fish_user_paths $HOME"/bin" $fish_user_paths
-
-echo "hi"
