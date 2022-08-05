17933972844d0ad8d5de8a5ece4b811046099c34
Carl Tashian <carl@smallstep.com>
2022-08-05 10:54:09 -0700

    add config command to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	alias grep='grep --color=auto'
+
+	# Dotfiles, stored via https://www.atlassian.com/git/tutorials/dotfiles
+	alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 end
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/carl/google-cloud-sdk/path.fish.inc' ]; . '/Users/carl/google-cloud-sdk/path.fish.inc'; end
+
 
 # we don't use the fish_add_path command here because 
 # it won't prepend to $PATH if we do.
