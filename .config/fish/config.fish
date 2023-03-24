ca496c7af8902bf21f9cf8a13ec5773407ced251
Carl Tashian <carl@smallstep.com>
2023-03-24 11:39:45 -0700

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,11 @@ if [ -f '/Users/carl/google-cloud-sdk/path.fish.inc' ]; . '/Users/carl/google-cl
 
 # we don't use the fish_add_path command here because 
 # it won't prepend to $PATH if we do.
-set -g fish_user_paths $HOME"/bin" $fish_user_paths
+set -g fish_user_paths $HOME"/bin" $HOME"/go/bin" "/opt/homebrew/sbin" $fish_user_paths
+
+# pnpm
+set -gx PNPM_HOME "/Users/carl/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
